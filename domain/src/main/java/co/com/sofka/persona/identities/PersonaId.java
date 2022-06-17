dc5ddb94da731568362e44673a59da56a4c20d33
Steven Calderon <scalderon1504@gmail.com>
2022-06-17 11:48:33 -0500

    Se mejora el identity de persona

--- domain/src/main/java/co/com/sofka/persona/identities/PersonaId.java
+++ domain/src/main/java/co/com/sofka/persona/identities/PersonaId.java
@@ -3,5 +3,14 @@ package co.com.sofka.persona.identities;
 import co.com.sofka.domain.generic.Identity;
 
 public class PersonaId extends Identity {
+    private PersonaId(String id) {
+        super(id);
+    }
 
+    public static PersonaId of(String id) {
+        return new PersonaId(id);
+    }
+
+    public PersonaId() {
+    }
 }
