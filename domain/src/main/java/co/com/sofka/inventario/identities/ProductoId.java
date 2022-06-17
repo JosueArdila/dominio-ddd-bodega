f0036c048ec4ac25deee3697005afacd52dde94b
Steven Calderon <scalderon1504@gmail.com>
2022-06-17 11:48:19 -0500

    Se mejora el identity de persona

--- domain/src/main/java/co/com/sofka/inventario/identities/ProductoId.java
+++ domain/src/main/java/co/com/sofka/inventario/identities/ProductoId.java
@@ -3,4 +3,14 @@ package co.com.sofka.inventario.identities;
 import co.com.sofka.domain.generic.Identity;
 
 public class ProductoId extends Identity {
+    private ProductoId(String id) {
+        super(id);
+    }
+
+    public ProductoId() {
+    }
+
+    public static ProductoId of(String id) {
+        return new ProductoId(id);
+    }
 }
